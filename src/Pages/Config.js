2d6cef08ceaf19c7dcdfc1099858bcf19b626e2a
rafaelftourinho <rafaelftourinho@gmail.com>
2022-09-08 16:23:33 -0300

    :sparkles: Req 3

---
+++ src/Pages/Config.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Config extends Component {
+  render() {
+    return (
+      <div data-testid="settings-title">Config</div>
+    );
+  }
+}
+
+export default Config;
