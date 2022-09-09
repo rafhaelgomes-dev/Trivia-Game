623cf64ea94f80cb192ec8beb58a0a201f5aa827
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-09-09 19:02:38 -0300

    requisito 6

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -9,6 +9,8 @@ class Game extends Component {
     this.state = {
       index: 0,
       clickAnswer: false,
+      botoesOrdemAtual: undefined,
+      botoesOrdemAtual2: undefined,
     };
   }
 
@@ -18,48 +20,26 @@ class Game extends Component {
     if (Results.response_code === responseCode) {
       return history.push('/');
     }
+    const array = this.shuffleArray();
+    this.setState({
+      botoesOrdemAtual: array,
+    });
   }
 
-  shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i -= 1) {
-      // Generate random number
-      const j = Math.floor(Math.random() * (i + 1));
-
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-
-    return array;
-  };
-
-  handleIncrementIndex = async () => {
-    const number = 4;
-    await this.setState((prevState) => ({ index: prevState.index === number
-      ? prevState.index : prevState.index + 1,
-    clickAnswer: false,
-    }));
-
-    // FUNÇÃO QUE REDIRECIONA PARA O FEEDBACK
-    // const { index } = this.state;
-    // if (index === number) {
-    //   const { history } = this.props;
-    //   history.push('/feedback');
-    // }
-  };
-
-  handleColor = () => {
-    this.setState({
-      clickAnswer: true,
-    });
-  };
-
-  handlMultipleQuestions = () => {
-    const { Results } = this.props;
+  shuffleArray = () => {
     const { index, clickAnswer } = this.state;
     const colorWrongAnswer = '3px solid red';
     const colorCorrectAnswer = '3px solid rgb(6, 240, 15)';
-    const botoes = [
+    const { Results } = this.props;
+    if (Results.results[index].incorrect_answers.length === 1) {
+      console.log(Results.results[index].incorrect_answers.length);
+      const array = this.shuffleArray2();
+      console.log(array);
+      this.setState({
+        botoesOrdemAtual2: array,
+      });
+    }
+    const array = [
       <button
         type="button"
         key="0"
@@ -100,25 +80,23 @@ class Game extends Component {
 
       </button>,
     ];
-    const arrVirado = this.shuffleArray(botoes);
-    return (
-      <div data-testid="answer-options">
-        <p data-testid="question-category">{Results.results[index].category}</p>
-        <p data-testid="question-text">{Results.results[index].question}</p>
-        {arrVirado[0]}
-        {arrVirado[1]}
-        {arrVirado[2]}
-        {arrVirado[3]}
-      </div>
-    );
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      // Generate random number
+      const j = Math.floor(Math.random() * (i + 1));
+
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
   };
 
-  handleBooleanQuestions = () => {
-    const { Results } = this.props;
+  shuffleArray2 = () => {
     const { index, clickAnswer } = this.state;
     const colorWrongAnswer = '3px solid red';
     const colorCorrectAnswer = '3px solid rgb(6, 240, 15)';
-    const botoes = [
+    const { Results } = this.props;
+    const array = [
       <button
         type="button"
         key="0"
@@ -137,32 +115,80 @@ class Game extends Component {
         onClick={ this.handleColor }
       >
         {Results.results[index].incorrect_answers[0]}
-
       </button>,
     ];
-    const arrVirado = this.shuffleArray(botoes);
-    return (
-      <div data-testid="answer-options">
-        <p data-testid="question-category">{Results.results[index].category}</p>
-        <p data-testid="question-text">{Results.results[index].question}</p>
-        {arrVirado[0]}
-        {arrVirado[1]}
-      </div>
-    );
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      // Generate random number
+      const j = Math.floor(Math.random() * (i + 1));
+
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+
+  handleIncrementIndex = async () => {
+    const number = 4;
+    await this.setState((prevState) => ({ index: prevState.index === number
+      ? prevState.index : prevState.index + 1,
+    clickAnswer: false,
+    }));
+    const array = this.shuffleArray();
+    this.setState({
+      botoesOrdemAtual: array,
+    });
+
+    // FUNÇÃO QUE REDIRECIONA PARA O FEEDBACK
+    // const { index } = this.state;
+    // if (index === number) {
+    //   const { history } = this.props;
+    //   history.push('/feedback');
+    // }
+  };
+
+  handleColor = () => {
+    this.setState({
+      clickAnswer: true,
+    });
   };
 
   render() {
     const { Results } = this.props;
-    const { index, clickAnswer } = this.state;
+    const { index, clickAnswer, botoesOrdemAtual, botoesOrdemAtual2 } = this.state;
     const responseCode = 3;
+    if (botoesOrdemAtual === undefined) {
+      return <p>Carregando</p>;
+    }
     return (
       <section>
         <Header />
         <div>
           {Results.response_code !== responseCode && (
             Results.results[index].type === 'multiple'
-              ? this.handlMultipleQuestions() : (
-                this.handleBooleanQuestions()
+              ? (
+                <div data-testid="answer-options">
+                  <p data-testid="question-category">{Results.results[index].category}</p>
+                  <p data-testid="question-text">{Results.results[index].question}</p>
+                  {botoesOrdemAtual[0]}
+                  {botoesOrdemAtual[1]}
+                  {botoesOrdemAtual[2]}
+                  {botoesOrdemAtual[3]}
+                </div>
+              ) : (
+                (
+                  <div data-testid="answer-options">
+                    <p
+                      data-testid="question-category"
+                    >
+                      {Results.results[index].category}
+
+                    </p>
+                    <p data-testid="question-text">{Results.results[index].question}</p>
+                    {botoesOrdemAtual2[0]}
+                    {botoesOrdemAtual2[1]}
+                  </div>
+                )
               ))}
           {clickAnswer && (
             <button
