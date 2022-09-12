1aeedb71a2b9fdbcb503bde0324359423d60526a
rafaelftourinho <rafaelftourinho@gmail.com>
2022-09-11 21:32:48 -0300

    :sparkles: Req 18

--- src/Components/Ranking.js
+++ src/Components/Ranking.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Ranking extends Component {
+  handleClickHome = (event) => {
+    event.preventDefault();
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div data-testid="ranking-title">
-        <h1>Ranking</h1>
+        <h1> Ranking </h1>
+        <button
+          data-testid="btn-go-home"
+          type="submit"
+          onClick={ this.handleClickHome }
+        >
+          Home
+        </button>
       </div>
     );
   }
 }
 
-export default Ranking;
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Ranking);
